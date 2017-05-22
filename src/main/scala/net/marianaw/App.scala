73bc49eb9322153272036847d363aa204aef3623
marianaw <mariana.09.w@gmail.com>
2017-05-22 15:11:54 -0300

    First commit.

---
+++ src/main/scala/net/marianaw/App.scala
@@ -0,0 +1,17 @@
+package net.marianaw
+
+import cc.mallet.grmm.learning
+
+/**
+ * @author ${user.name}
+ */
+object App {
+  
+  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
+  
+  def main(args : Array[String]) {
+    println( "Hello World!" )
+    println("concat arguments = " + foo(args))
+  }
+
+}
